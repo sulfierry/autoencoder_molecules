a9b4c00d6d3b41d087dcfdb3c3eb2b8cf40a48d3
sulfierry <leon.bio0@hotmail.com>
2023-09-18 16:45:08 -0300

    3di

--- 3di.py
+++ 3di.py
@@ -43,3 +43,49 @@ class StructureTo3diSeqDist:
                         closestState = j
                         minDistance = distToCentroid
             states[i] = closestState
+
+
+class StructureTo3DiBase:
+
+    # Constantes
+    CENTROID_CNT = 20
+    INVALID_STATE = 2  # designa resíduos inválidos para o estado coil
+    DISTANCE_ALPHA_BETA = 1.5336
+    PI = 3.14159265359
+    FEATURE_CNT = 10
+    EMBEDDING_DIM = 2
+    VIRTUAL_CENTER = {"alpha": 270, "beta": 0, "d": 2}
+
+    centroids = [
+        [-1.0729, -0.3600],
+        [-0.1356, -1.8914],
+        [0.4948, -0.4205],
+        # ... (existem mais centróides que podem ser adicionados)
+    ]
+
+    # Operações vetoriais
+
+    @staticmethod
+    def add(a, b):
+        return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
+
+    @staticmethod
+    def sub(a, b):
+        return [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
+
+    @staticmethod
+    def norm(a):
+        length = (a[0] ** 2 + a[1] ** 2 + a[2] ** 2) ** 0.5
+        return [i / length for i in a]
+
+    @staticmethod
+    def cross(a, b):
+        return [a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]]
+
+    @staticmethod
+    def scale(a, f):
+        return [i * f for i in a]
+
+    @staticmethod
+    def dot(a, b):
+        return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
