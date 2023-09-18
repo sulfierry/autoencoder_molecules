b362cb1d12b7da6ffe07256297322f9b6c2e41bb
sulfierry <leon.bio0@hotmail.com>
2023-09-18 16:56:02 -0300

    3di

--- 3di.py
+++ 3di.py
@@ -1,7 +1,12 @@
+import math
+
 class Alphabet3diSeqDist:
     CENTROID_CNT = 20
     INVALID_STATE = CENTROID_CNT
     centroids = [-284,-147,-83,-52,-33,-21,-13,-7,-4,-3,-1,1,3,7,13,24,40,68,123,250]
+
+
+
 
 class StructureTo3diSeqDist:
     
@@ -9,6 +14,27 @@ class StructureTo3diSeqDist:
         self.states = []
         self.partnerIdx = []
         self.mask = []
+
+    @staticmethod
+    def createResidueMask(mask, ca, n, c, length):
+        for i in range(length):
+            mask[i] = not (math.isnan(ca[i][0]) or math.isnan(n[i][0]) or math.isnan(c[i][0]))
+
+    @staticmethod
+    def findResiduePartners(partnerIdx, cb, validMask, n):
+        # Escolhe para cada resíduo o vizinho mais próximo como parceiro
+        # em termos de distâncias entre seus centros virtuais/C_betas.
+        # Ignora os primeiros/últimos e resíduos inválidos.
+        for i in range(1, n - 1):
+            minDistance = float('inf')
+            for j in range(1, n - 1):
+                if i != j and validMask[j]:
+                    dist = StructureTo3Di.calcDistanceBetween(cb[i], cb[j])
+                    if dist < minDistance:
+                        minDistance = dist
+                        partnerIdx[i] = j
+            if partnerIdx[i] == -1:  # nenhum parceiro encontrado
+                validMask[i] = False
             
     def structure2states(self, ca, n, c, cb, length):
         self.states.clear()
@@ -19,6 +45,7 @@ class StructureTo3diSeqDist:
             self.states = [-1] * length
             self.partnerIdx = [-1] * length
             self.mask = [False] * length
+
 
         # Estas funções precisam ser implementadas com base em suas implementações C++
         self.replaceCBWithVirtualCenter(ca, n, c, cb, length)
