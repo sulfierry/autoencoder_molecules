3f8ac89251385bf83aeb6aaa5e3690e0fbba8fd0
Leon Sulfierry <leon.bio0@hotmail.com>
2023-10-24 17:27:57 -0300

    Update autoecoder_chembl.py

--- autoecoder_chembl.py
+++ autoecoder_chembl.py
@@ -38,3 +38,27 @@ smiles_indices = [smiles_to_indices(smiles, char_to_index) for smiles in filtere
 # Converter listas de índices em tensores PyTorch e preencher sequências para terem o mesmo comprimento
 smiles_tensors = [torch.tensor(indices, dtype=torch.long) for indices in smiles_indices]
 smiles_padded = rnn_utils.pad_sequence(smiles_tensors, batch_first=True)
+
+# Parte 2: Pré-processar os Dados SMILES do ChEMBL
+# Calcular o comprimento de cada string SMILES
+smiles_lengths = df_chembl['canonical_smiles'].apply(len)
+
+# Filtrar o conjunto de dados para manter apenas moléculas com comprimento de SMILES <= 121 (percentil 95)
+threshold_95 = smiles_lengths.quantile(0.95)
+filtered_df_chembl = df_chembl[smiles_lengths <= threshold_95]
+
+# Obter todos os caracteres únicos nas strings SMILES
+unique_chars = set(''.join(filtered_df_chembl['canonical_smiles']))
+char_to_index = {char: i for i, char in enumerate(sorted(unique_chars))}
+
+# Função para converter uma string SMILES em uma lista de índices inteiros
+def smiles_to_indices(smiles, char_to_index):
+    return [char_to_index[char] for char in smiles]
+
+# Converter todas as strings SMILES filtradas em listas de índices inteiros
+smiles_indices = [smiles_to_indices(smiles, char_to_index) for smiles in filtered_df_chembl['canonical_smiles']]
+
+# Converter listas de índices em tensores PyTorch e preencher sequências para terem o mesmo comprimento
+smiles_tensors = [torch.tensor(indices, dtype=torch.long) for indices in smiles_indices]
+smiles_padded = rnn_utils.pad_sequence(smiles_tensors, batch_first=True)
+
