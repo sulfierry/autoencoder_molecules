807da17624c2e5dd4825e22be66f72a5521ec9be
Leon Sulfierry <leon.bio0@hotmail.com>
2023-10-25 18:50:22 -0300

    Update ver2.py

--- ver2.py
+++ ver2.py
@@ -10,7 +10,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 import torch.nn.functional as F
-
+from torch.utils.data import DataLoader, TensorDataset
 
 class MoleculeSimilarityFinder:
     def __init__(self, chembl_file_path, pkidb_file_path, embedding_dim=10, latent_dim=32, num_epochs=10, batch_size=32):
@@ -19,9 +19,6 @@ class MoleculeSimilarityFinder:
 
         self.pkidb_file_path = pkidb_file_path
         print("Dados PKIDB carregados com sucesso.")
-        
-        self.dense_layer = nn.Linear(latent_dim, embedding_dim)  # Camada densa adicionada
-        self.layer_norm = nn.LayerNorm(latent_dim)  # Camada de normalização adicionada
 
         self.embedding_dim = embedding_dim
         self.latent_dim = latent_dim
@@ -89,7 +86,7 @@ class MoleculeSimilarityFinder:
             smiles_indices.append(indices)
     
         # Converter listas de índices em tensores PyTorch e preencher sequências para terem o mesmo comprimento
-        smiles_tensors = [torch.tensor(indices, dtype=torch.long) for indices in smiles_indices]  # Alterado para float
+        smiles_tensors = [torch.tensor(indices, dtype=torch.long) for indices in smiles_indices]
         smiles_padded = rnn_utils.pad_sequence(smiles_tensors, batch_first=True, padding_value=char_to_index["<pad>"])
 
         print("SMILES convertidos para tensor com sucesso.")
@@ -98,27 +95,29 @@ class MoleculeSimilarityFinder:
 
     def convert_indices_to_embeddings(self, indices_tensor):
         with torch.no_grad():
-            embeddings = self.model.embedding(indices_tensor.long())  # Adicionado .long() para converter para inteiros
+            embeddings = self.model.embedding(indices_tensor.long())
   
         print("Índices convertidos para embeddings com sucesso.")
 
         return embeddings
-        
+
 
     def define_and_train_autoencoder(self, smiles_padded, vocab_size):
         print("Definindo e treinando o autoencoder...")
-        # Definir a arquitetura do autoencoder
+    
+        # Definir o dataset e o DataLoader
+        dataset = TensorDataset(smiles_padded)
+        data_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+        
+        # Definir o modelo
         self.model = Autoencoder(vocab_size, self.embedding_dim, self.latent_dim)
-
-        # Definir critério de perda e otimizador
-        criterion = nn.MSELoss()
-        optimizer = optim.Adam(self.model.parameters(), lr=0.001)
-
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)  # Agendador de taxa de aprendizado
-
-        # Preparação para treinamento com mini-batches
-        dataset = torch.utils.data.TensorDataset(smiles_padded)
-        data_loader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+        
+        # Definir a função de perda e o otimizador
+        criterion = nn.CrossEntropyLoss(ignore_index=0)  # Ignorar o índice de preenchimento na perda
+        optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        
+        # Opcional: Definir um scheduler para ajustar a taxa de aprendizado
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
         
         # Treinar o modelo
         latent_features = []
@@ -126,103 +125,89 @@ class MoleculeSimilarityFinder:
             total_loss = 0
             for smiles_batch in tqdm(data_loader, desc=f'Epoch {epoch+1}/{self.num_epochs}'):
                 smiles_batch = smiles_batch[0]
-                smiles_batch = self.convert_indices_to_embeddings(smiles_batch)  # Adicionado esta linha
-                optimizer.zero_grad()
+                
+                # Garanta que as operações estejam no modo de treinamento
+                self.model.train()
                 reconstructed, z = self.model(smiles_batch)
+                
+                # Ajustar tamanho das dimensões, se necessário
+                if reconstructed.shape[1] != smiles_batch.shape[1]:
+                    reconstructed = reconstructed[:, :smiles_batch.shape[1], :]
+    
+                # Aplique view para corresponder às dimensões esperadas pela CrossEntropyLoss
+                smiles_batch = smiles_batch.view(-1)
+                reconstructed = reconstructed.view(-1, reconstructed.size(-1))
+    
+                # Agora, você pode calcular a perda já que os tensores têm a mesma forma
                 loss = criterion(reconstructed, smiles_batch)
                 loss.backward()
                 optimizer.step()
+                optimizer.zero_grad()
+                
                 total_loss += loss.item()
-                latent_features.append(z.detach().numpy())
-                scheduler.step()  # Ajustar taxa de aprendizado a cada época
-
+                latent_features.append(z.cpu().detach().numpy())
     
+            scheduler.step()
             print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_epochs, total_loss / len(data_loader)))
-    
-        print("Treinamento concluído.")
         
-        return np.vstack(latent_features)
+        # Salvar os recursos latentes
+        latent_features = np.concatenate(latent_features, axis=0)
+        print("Autoencoder treinado com sucesso.")
+        return latent_features
+    
+    
+    def find_similar_molecules(self, latent_features_chembl, latent_features_pkidb, n_top_molecules=5):
+        print("Encontrando moléculas similares...")
         
-    def find_similar_molecules(self):
-        print("Iniciando busca por moléculas similares...")
- 
-        # Parte 2: Pré-processar os Dados SMILES do ChEMBL
-        print("Preprocessando dados SMILES do ChEMBL...")
-
-        self.filtered_df_chembl, self.char_to_index, threshold_95 = self.preprocess_smiles_data(self.df_chembl, 'canonical_smiles')
-    
-        vocab_size = len(self.char_to_index)
-        print("Tamanho do vocabulário:", vocab_size)
-
-        print("Convertendo SMILES para tensor...")
-        try:
-            smiles_padded = self.smiles_to_tensor(self.filtered_df_chembl, 'canonical_smiles', self.char_to_index, vocab_size)
-            print("Conversão de SMILES para tensor concluída com sucesso.")
-        except Exception as e:
-            print("Ocorreu um erro durante a conversão de SMILES para tensor:", str(e))
-            return
-            
-        # Verificar se os índices estão no intervalo válido
-        max_index = smiles_padded.max().item()
-        if max_index >= vocab_size:
-            print("Erro: Índice máximo é maior ou igual ao tamanho do vocabulário.")
-            return
-
-        # Parte 3: Definir e Treinar o Autoencoder
-        self.latent_features_chembl = self.define_and_train_autoencoder(smiles_padded, vocab_size)
-        # Nota: Aqui, o treinamento é feito com o conjunto de dados ChEMBL, mas o mesmo modelo pode ser aplicado ao conjunto de dados PKIDB
-    
-        # Parte 4: Preprocessar e Converter os Dados SMILES do PKIDB
-        print("Preprocessando e convertendo dados do PKIDB...")
+        # Calcular as semelhanças de cosseno
+        similarities = cosine_similarity(latent_features_chembl, latent_features_pkidb)
+        
+        # Encontrar as moléculas mais similares
+        top_molecule_indices = np.argsort(similarities, axis=0)[-n_top_molecules:]
+        
+        print("Moléculas similares encontradas com sucesso.")
+       
+        return top_molecule_indices    
+       
+    def run(self):
+        print("Iniciando o processo de busca por similaridade de moléculas...")
+        
+        # Preprocessar os dados SMILES
+        self.filtered_df_chembl, self.char_to_index, max_len = self.preprocess_smiles_data(self.df_chembl, 'canonical_smiles')
         self.filtered_df_pkidb, _, _ = self.preprocess_smiles_data(self.df_pkidb, 'Canonical_Smiles')
-        smiles_padded_pkidb = self.smiles_to_tensor(self.filtered_df_pkidb, 'Canonical_Smiles', self.char_to_index, vocab_size)
-    
-        # Parte 5: Obter Representações Latentes das Moléculas do PKIDB
-        _, latent_features_pkidb = self.model(smiles_padded_pkidb)
-    
-        # Parte 6: Calcular Similaridade de Cosseno entre as Representações Latentes
-        similarity_matrix = cosine_similarity(self.latent_features_chembl.detach().numpy(), latent_features_pkidb.detach().numpy())
-        
-        # Parte 7: Identificar Moléculas Similares
-        similar_molecules = np.where(similarity_matrix > 0.95)  # Ajuste este valor conforme necessário
-        for chembl_index, pkidb_index in zip(*similar_molecules):
-            chembl_smiles = self.filtered_df_chembl.iloc[chembl_index]['canonical_smiles']
-            pkidb_smiles = self.filtered_df_pkidb.iloc[pkidb_index]['Canonical_Smiles']
-            print(f"Molécula similar encontrada: {chembl_smiles} (ChEMBL) e {pkidb_smiles} (PKIDB)")
-            
-        print("Busca por moléculas similares concluída.")
 
+        # Converter os dados SMILES em tensores
+        vocab_size = len(self.char_to_index)
+        smiles_padded_chembl = self.smiles_to_tensor(self.filtered_df_chembl, 'canonical_smiles', self.char_to_index, vocab_size)
+        smiles_padded_pkidb = self.smiles_to_tensor(self.filtered_df_pkidb, 'Canonical_Smiles', self.char_to_index, vocab_size)
+
+        # Definir e treinar o autoencoder
+        latent_features_chembl = self.define_and_train_autoencoder(smiles_padded_chembl, vocab_size)
+        latent_features_pkidb = self.define_and_train_autoencoder(smiles_padded_pkidb, vocab_size)
+
+        # Encontrar moléculas similares
+        top_molecule_indices = self.find_similar_molecules(latent_features_chembl, latent_features_pkidb)
+        
+        print("Processo concluído com sucesso.")
+        return top_molecule_indices
 
 class Autoencoder(nn.Module):
-    
     def __init__(self, vocab_size, embedding_dim, latent_dim):
         super(Autoencoder, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=vocab_size-1)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
         self.encoder = nn.LSTM(embedding_dim, latent_dim, batch_first=True)
         self.decoder = nn.LSTM(latent_dim, embedding_dim, batch_first=True)
-        self.linear = nn.Linear(embedding_dim, vocab_size)  # Adicionado
+        self.linear = nn.Linear(embedding_dim, vocab_size)
         
     def forward(self, x):
-        if x.max().item() >= self.embedding.num_embeddings:
-            raise IndexError("Índice fora do alcance. Certifique-se de que todos os índices são menores que o tamanho do vocabulário.")
-        x = x.long()  # Garantindo que x é do tipo long
-        x_embedded = self.embedding(x)
-        _, (h_n, _) = self.encoder(x_embedded)
-        z = h_n.squeeze(0)
-        
-        z = F.relu(self.dense_layer(z))  # Aplicando transformação linear e função de ativação
-        z = self.layer_norm(z)  # Aplicando normalização da camada
-        
-        # Repita z para ter a mesma sequência de comprimento que x
-        z_repeated = z.unsqueeze(1).repeat(1, x.size(1), 1)
-        
-        # Decodifique
-        x_reconstructed, _ = self.decoder(z_repeated)
-        
-        # Transformar de volta para o espaço original
-        x_reconstructed = self.linear(x_reconstructed)
+        embeddings = self.embedding(x)
+        _, (hn, _) = self.encoder(embeddings)
+        reconstructions, _ = self.decoder(hn.repeat(1, x.size(1), 1))
+        reconstructions = reconstructions.contiguous().view(-1, reconstructions.size(-1))
+        reconstructions = self.linear(reconstructions)
+        reconstructions = reconstructions.view(x.size(0), -1, reconstructions.size(-1))
+        return reconstructions, hn.squeeze(0)
 
-        return x_reconstructed, z
 
 
 if __name__ == "__main__":
