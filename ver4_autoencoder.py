e9890c7702c75e0d1cdf518dc82beff98c58b7ca
Leon Sulfierry <leon.bio0@hotmail.com>
2023-10-31 17:54:01 -0300

    Update ver4_autoencoder.py

--- ver4_autoencoder.py
+++ ver4_autoencoder.py
@@ -90,3 +90,8 @@ def plot_histogram(similarity_scores):
     plt.xlabel("Similarity Score")
     plt.ylabel("Number of Molecules")
     plt.show()
+
+
+def save_similar_molecules_to_tsv(similar_molecules_info, file_path):
+    df = pd.DataFrame(similar_molecules_info)
+    df.to_csv(file_path, sep='\t', index=False)
