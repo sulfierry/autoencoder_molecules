9762aa16ca9435d1a5466ea1065e3ac4ab279a9e
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-01 17:15:45 -0300

    Update ver_0.5_pkidb_chembl.py
    class MoelculeFinder

--- ver_0.5_pkidb_chembl.py
+++ ver_0.5_pkidb_chembl.py
@@ -11,3 +11,129 @@ from transformers import RobertaModel, RobertaTokenizer
 
 # Definindo o dispositivo como GPU (CUDA) se disponível, senão será CPU
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class MoleculeSimilarityFinder:
+    def __init__(self, chembl_file_path, pkidb_file_path, device=device):
+        """
+        Inicializa o objeto MoleculeSimilarityFinder.
+
+        :param chembl_file_path: Caminho para o arquivo CSV que contém dados do ChEMBL.
+        :param pkidb_file_path: Caminho para o arquivo CSV que contém dados do PKIDB.
+        :param device: Dispositivo no qual rodar o modelo (CPU ou CUDA).
+        """
+        self.chembl_file_path = chembl_file_path
+        self.pkidb_file_path = pkidb_file_path
+        self.device = device
+
+        # Carrega o tokenizador e o modelo ChemBERTa pré-treinado
+        self.tokenizer = RobertaTokenizer.from_pretrained('seyonec/ChemBERTa-zinc-base-v1')
+        self.model = RobertaModel.from_pretrained('seyonec/ChemBERTa-zinc-base-v1')
+
+        # Move o modelo para o dispositivo especificado
+        self.model.to(device)
+
+
+    def get_molecule_embedding(self, smiles_list, inner_batch_size=32):
+        """
+        Gera embeddings para uma lista de moléculas representadas como SMILES.
+
+        :param smiles_list: Lista de strings SMILES representando moléculas.
+        :return: Embeddings das moléculas.
+        """
+        all_embeddings = []
+        for i in range(0, len(smiles_list), inner_batch_size):
+            smiles_batch = smiles_list[i:i + inner_batch_size]
+
+            # Tokeniza os SMILES e move os tokens para o dispositivo especificado
+            tokens = self.tokenizer(smiles_batch, return_tensors='pt', padding=True, truncation=True, max_length=512)
+            tokens = {key: val.to(self.device) for key, val in tokens.items()}
+
+            # Gera as embeddings sem calcular gradientes (para economizar memória e acelerar)
+            with torch.no_grad():
+                outputs = self.model(**tokens)
+
+            # Calcula a média das embeddings em todas as posições para obter um único vetor por molécula
+            embeddings = outputs.last_hidden_state.mean(dim=1)
+            all_embeddings.append(embeddings.cpu())  # Move embeddings to CPU to save GPU memory
+
+            del tokens, outputs, embeddings
+            torch.cuda.empty_cache()
+
+        return torch.cat(all_embeddings, dim=0)
+
+
+    def find_similar_molecules(self, threshold=0.8, batch_size=64, inner_batch_size=32):
+        """
+        Encontra moléculas similares com base em embeddings moleculares.
+
+        :param threshold: Limiar de similaridade para considerar as moléculas como similares
+        :param batch_size: Número de moléculas a serem processadas por lote
+        :param inner_batch_size: Tamanho do lote interno para processamento de embeddings
+        :return: Uma lista de pontuações de similaridade e uma lista de informações sobre as moléculas similares encontradas
+        """
+
+        # Carregar os dados do ChEMBL e PKIDB
+        chembl_data = pd.read_csv(self.chembl_file_path, sep='\t')
+        pkidb_data = pd.read_csv(self.pkidb_file_path, header=None, names=['pkidb_smile'])
+
+        # Converter as colunas de SMILES em listas para processamento em lote
+        chembl_smiles = chembl_data['canonical_smiles'].tolist()
+        pkidb_smiles = pkidb_data['pkidb_smile'].tolist()
+
+        # Inicializar listas para armazenar informações sobre moléculas similares e pontuações de similaridade
+        similar_molecules_info = []
+        similarity_scores = []
+
+        # Loop sobre os lotes de moléculas do PKIDB
+        for i in tqdm(range(0, len(pkidb_smiles), batch_size), desc="Processing PKIDB molecules"):
+            # Selecionar o lote atual de SMILES do PKIDB
+            pkidb_batch = pkidb_smiles[i:i + batch_size]
+
+            # Calcular embeddings para o lote atual de moléculas do PKIDB
+            with torch.no_grad():  # Desativar cálculo de gradientes para economizar memória
+                pkidb_embeddings = self.get_molecule_embedding(pkidb_batch, inner_batch_size=inner_batch_size).cpu().numpy()
+
+            # Normalizar os embeddings para calcular a similaridade do cosseno
+            faiss.normalize_L2(pkidb_embeddings)
+
+            # Loop sobre os lotes de moléculas do ChEMBL
+            for j in tqdm(range(0, len(chembl_smiles), batch_size), desc="Processing ChEMBL molecules", leave=False):
+                # Selecionar o lote atual de SMILES do ChEMBL
+                chembl_batch = chembl_smiles[j:j + batch_size]
+
+                # Calcular embeddings para o lote atual de moléculas do ChEMBL
+                with torch.no_grad():
+                    chembl_embeddings = self.get_molecule_embedding(chembl_batch, inner_batch_size=inner_batch_size).cpu().numpy()
+
+                # Normalizar os embeddings
+                faiss.normalize_L2(chembl_embeddings)
+
+                # Criar um índice FAISS para buscar moléculas similares
+                index = faiss.IndexFlatIP(chembl_embeddings.shape[1])
+                index.add(chembl_embeddings)
+
+                # Procurar as moléculas mais similares do ChEMBL para cada molécula do PKIDB
+                D, I = index.search(pkidb_embeddings, k=chembl_embeddings.shape[0])
+
+                # Loop sobre os resultados da busca
+                for k, (indices, distances) in enumerate(zip(I, D)):
+                    for idx, score in zip(indices, distances):
+                        if score > threshold:
+                            # Se a similaridade for maior que o limiar, adicione as informações à lista
+                            similar_molecules_info.append({
+                                'molregno': chembl_data.loc[j + idx, 'molregno'],
+                                'chembl_smile': chembl_batch[idx],
+                                'pkidb_smile': pkidb_batch[k],
+                                'similarity': score
+                            })
+                            similarity_scores.append(score)
+
+                # Liberar a memória dos tensores e da GPU
+                del chembl_embeddings
+                torch.cuda.empty_cache()
+
+            # Liberar a memória dos tensores e da GPU
+            del pkidb_embeddings
+            torch.cuda.empty_cache()
+
+        return similarity_scores, similar_molecules_info
