3eee46f6efd57f682324099c9da6756ccf532bd8
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-03 14:24:49 -0300

    Update and rename ver_06.py to ver_06_pkidb_chembl.py

--- ver_06_pkidb_chembl.py
+++ ver_06_pkidb_chembl.py
@@ -72,3 +72,17 @@ class MoleculeSimilarityFinder:
                             similarity_scores.append(score)
 
         return similarity_scores, similar_molecules_info
+
+
+class MoleculeVisualization:
+    @staticmethod
+    def plot_histogram(similarity_scores):
+        plt.figure(figsize=(10, 6))
+        plt.hist(similarity_scores, bins=50, color='skyblue', edgecolor='black', alpha=0.7)
+        plt.title("Distribuição dos Scores de Similaridade")
+        plt.xlabel("Score de Similaridade")
+        plt.ylabel("Número de Moléculas")
+        plt.grid(axis='y', alpha=0.75)
+        plt.savefig('distribution_of_similarity_scores.png')
+        plt.show()
+        plt.close()
