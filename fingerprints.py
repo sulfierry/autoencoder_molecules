ebfd98652a96c286a2e545022d8467c83bf8d3c8
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-05 15:37:58 -0300

    Update fingerprints.py

--- fingerprints.py
+++ fingerprints.py
@@ -3,3 +3,25 @@ from rdkit.Chem import AllChem, DataStructs
 import pandas as pd
 from concurrent.futures import ProcessPoolExecutor
 import os
+
+
+# Função para processar um chunk de dados
+def process_chunk(chunk, threshold):
+    # Converter SMILES em objetos de molécula RDKit e calcular fingerprints
+    chembl_molecules = (Chem.MolFromSmiles(smile) for smile in chunk['chembl_smile'])
+    pkidb_molecules = (Chem.MolFromSmiles(smile) for smile in chunk['pkidb_smile'])
+    
+    chembl_fps = (AllChem.GetMorganFingerprintAsBitVect(mol, radius=2) for mol in chembl_molecules if mol)
+    pkidb_fps = (AllChem.GetMorganFingerprintAsBitVect(mol, radius=2) for mol in pkidb_molecules if mol)
+    
+    # Calcular as similaridades de Tanimoto
+    tanimoto_similarities = [DataStructs.FingerprintSimilarity(chembl_fp, pkidb_fp)
+                             for chembl_fp, pkidb_fp in zip(chembl_fps, pkidb_fps)]
+    
+    # Adicionar as similaridades de Tanimoto ao chunk DataFrame
+    chunk['tanimoto_similarity'] = tanimoto_similarities
+    
+    # Filtrar com base no limiar de similaridade de Tanimoto
+    filtered_chunk = chunk[chunk['tanimoto_similarity'] >= threshold]
+    
+    return chunk, filtered_chunk
