b696a4cdbd7721339f8a08fb3814718d9488be6b
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-07 10:31:06 -0300

    Update ver_07_pkidb_chembl.py

--- ver_07_pkidb_chembl.py
+++ ver_07_pkidb_chembl.py
@@ -45,3 +45,47 @@ class MoleculeSimilarityFinder:
             return np.load(file_path)
         else:
             return None
+
+
+    def find_similar_molecules(self, target_file_path, chembl_embeddings_path, threshold=0.8, batch_size=64, inner_batch_size=32):
+        dtype = {'canonical_smiles': 'string', 'molregno': 'int32'}
+        chembl_data = pd.read_csv(self.chembl_file_path, sep='\t', dtype=dtype)
+        target_data = pd.read_csv(target_file_path, header=None, names=['target_smile'], dtype={'target_smile': 'string'})
+
+        chembl_smiles = chembl_data['canonical_smiles'].tolist()
+        target_smiles = target_data['target_smile'].tolist()
+
+        similar_molecules_info = []
+        similarity_scores = []
+
+        # Tente carregar os embeddings do ChEMBL salvos previamente
+        chembl_embeddings = self.load_embeddings(chembl_embeddings_path)
+        if chembl_embeddings is None:
+            # Se não existirem embeddings salvos, calcule-os e salve
+            chembl_embeddings = self.get_molecule_embedding(chembl_smiles, inner_batch_size=inner_batch_size).to('cpu').numpy()
+            self.save_embeddings(chembl_embeddings, chembl_embeddings_path)
+        faiss.normalize_L2(chembl_embeddings)
+
+        for i in tqdm(range(0, len(target_smiles), batch_size), desc="Processing target molecules"):
+            target_batch = target_smiles[i:i + batch_size]
+            target_embeddings = self.get_molecule_embedding(target_batch, inner_batch_size=inner_batch_size).to('cpu').numpy()  # Mova para a CPU antes de converter para numpy
+            faiss.normalize_L2(target_embeddings)
+
+            index = faiss.IndexFlatIP(chembl_embeddings.shape[1])
+            index.add(chembl_embeddings)
+
+            D, I = index.search(target_embeddings, k=chembl_embeddings.shape[0])
+
+            for k, (indices, distances) in enumerate(zip(I, D)):
+                for idx, score in zip(indices, distances):
+                    if score > threshold:
+                        similar_molecules_info.append({
+                            'molregno': chembl_data.iloc[idx]['molregno'],
+                            'chembl_smile': chembl_smiles[idx],
+                            'target_smile': target_batch[k],
+                            'similarity': score
+                        })
+                        similarity_scores.append(score)
+
+        return similarity_scores, similar_molecules_info
+
