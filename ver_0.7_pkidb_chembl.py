8604ab6a0489a26b4c3db53e306615054fcfd13b
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-08 10:52:48 -0300

    Update ver_0.7_pkidb_chembl.py

--- ver_0.7_pkidb_chembl.py
+++ ver_0.7_pkidb_chembl.py
@@ -10,9 +10,12 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from transformers import RobertaModel, RobertaTokenizer
-import os
+from multiprocessing import cpu_count, Pool
+import torch.multiprocessing as mp
+
 
 # Definindo o dispositivo como GPU (CUDA) se disponível, senão será CPU
+mp.set_start_method('spawn', force=True)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class MoleculeSimilarityFinder:
@@ -23,7 +26,10 @@ class MoleculeSimilarityFinder:
 
         self.tokenizer = RobertaTokenizer.from_pretrained('seyonec/ChemBERTa-zinc-base-v1')
         self.model = RobertaModel.from_pretrained('seyonec/ChemBERTa-zinc-base-v1')
-        self.model = torch.nn.DataParallel(self.model).to(device)
+        if device == "cuda":
+            self.model = torch.nn.DataParallel(self.model).to(device)
+        else:
+            self.model = self.model.to(device)
 
     def get_molecule_embedding(self, smiles_list, inner_batch_size=32):
         all_embeddings = []
@@ -34,18 +40,17 @@ class MoleculeSimilarityFinder:
                 tokens = {key: val.to(self.device) for key, val in tokens.items()}
                 outputs = self.model(**tokens)
                 embeddings = outputs.last_hidden_state.mean(dim=1)
-                all_embeddings.append(embeddings)  # Mantenha os embeddings na GPU
+                all_embeddings.append(embeddings.cpu())  # Transfer embeddings to CPU after calculation
         return torch.cat(all_embeddings, dim=0)
 
     def save_embeddings(self, embeddings, file_path):
-        np.save(file_path, embeddings)
+        np.save(file_path, embeddings.cpu().numpy())
 
     def load_embeddings(self, file_path):
         if os.path.exists(file_path):
             return np.load(file_path)
         else:
             return None
-
 
     def find_similar_molecules(self, target_file_path, chembl_embeddings_path, threshold=0.8, batch_size=64, inner_batch_size=32):
         dtype = {'canonical_smiles': 'string', 'molregno': 'int32'}
@@ -61,14 +66,19 @@ class MoleculeSimilarityFinder:
         # Tente carregar os embeddings do ChEMBL salvos previamente
         chembl_embeddings = self.load_embeddings(chembl_embeddings_path)
         if chembl_embeddings is None:
-            # Se não existirem embeddings salvos, calcule-os e salve
-            chembl_embeddings = self.get_molecule_embedding(chembl_smiles, inner_batch_size=inner_batch_size).to('cpu').numpy()
+            # Se não existirem embeddings salvos, calcule-os em paralelo e salve
+            chembl_embeddings = parallel_compute_embeddings(chembl_smiles, self)
             self.save_embeddings(chembl_embeddings, chembl_embeddings_path)
+        elif isinstance(chembl_embeddings, torch.Tensor):
+            # Assegure-se de que os embeddings são tensores PyTorch e estão na CPU
+            chembl_embeddings = chembl_embeddings.cpu().numpy()
+        # Não é necessário fazer nada se chembl_embeddings já for um numpy.ndarray
         faiss.normalize_L2(chembl_embeddings)
 
         for i in tqdm(range(0, len(target_smiles), batch_size), desc="Processing target molecules"):
             target_batch = target_smiles[i:i + batch_size]
-            target_embeddings = self.get_molecule_embedding(target_batch, inner_batch_size=inner_batch_size).to('cpu').numpy()  # Mova para a CPU antes de converter para numpy
+            # Calcule os embeddings em paralelo
+            target_embeddings = parallel_compute_embeddings(target_batch, self)
             faiss.normalize_L2(target_embeddings)
 
             index = faiss.IndexFlatIP(chembl_embeddings.shape[1])
@@ -88,124 +98,4 @@ class MoleculeSimilarityFinder:
                         similarity_scores.append(score)
 
         return similarity_scores, similar_molecules_info
-
-
-
-class MoleculeVisualization:
-    @staticmethod
-    def plot_histogram(similarity_scores):
-        plt.figure(figsize=(10, 6))
-        plt.hist(similarity_scores, bins=50, color='skyblue', edgecolor='black', alpha=0.7)
-        plt.title("Distribuição dos Scores de Similaridade")
-        plt.xlabel("Score de Similaridade")
-        plt.ylabel("Número de Moléculas")
-        plt.grid(axis='y', alpha=0.75)
-        plt.savefig('distribution_of_similarity_scores.png')
-        plt.show()
-        plt.close()
-
-
-    @staticmethod
-    def visualize_with_tsne(embeddings, labels):
-        tsne = TSNE(n_components=2, random_state=42)
-        tsne_results = tsne.fit_transform(embeddings)
-
-        plt.figure(figsize=(10, 8))
-        sns.scatterplot(x=tsne_results[:, 0], y=tsne_results[:, 1], hue=labels, palette="viridis", s=100, alpha=0.7)
-        plt.title('t-SNE Visualization of Molecule Embeddings')
-        plt.xlabel('t-SNE 1')
-        plt.ylabel('t-SNE 2')
-        plt.savefig('tsne_visualization_of_molecule_embeddings.png')
-        plt.show()
-        plt.close()
-
-
-    @staticmethod
-    def cluster_and_visualize(embeddings, num_clusters=5):
-        kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=10).fit(embeddings)
-        labels = kmeans.labels_
-
-        tsne = TSNE(n_components=2, random_state=42)
-        tsne_results = tsne.fit_transform(embeddings)
-
-        plt.figure(figsize=(10, 8))
-        sns.scatterplot(x=tsne_results[:, 0], y=tsne_results[:, 1], hue=labels, palette="viridis", s=100, alpha=0.7)
-        plt.title('Clustered Visualization of Molecule Embeddings')
-        plt.xlabel('t-SNE 1')
-        plt.ylabel('t-SNE 2')
-        plt.savefig('clustered_visualization_of_molecule_embeddings.png')
-        plt.show()
-        plt.close()
-
-
-    @staticmethod
-    def save_similar_molecules_to_tsv(similar_molecules_info, file_path):
-        df = pd.DataFrame(similar_molecules_info)
-        df.to_csv(file_path, sep='\t', index=False)
-
-
-def main():
-    # Iniciar o cronômetro
-    start_time = time.time()
-
-
-    chembl_file_path = '/content/drive/MyDrive/chemBERTA/mol_10000.tsv'
-    pkidb_file_path = '/content/drive/MyDrive/chemBERTA/smiles_from_pkidb_to_rdkit.tsv'
-    chembl_embeddings_path = '/content/drive/MyDrive/chemBERTA/XX_TIMER_ver7_chembl_embeddings.npy'
-    output_file_path = '/content/drive/MyDrive/chemBERTA/similar_molecules.tsv'
-    threshold = 0.8
-
-
-    print("Usando o dispositivo:", device)
-
-    # Carregar ou calcular embeddings do ChEMBL
-    if os.path.exists(chembl_embeddings_path):
-        print(f"Carregando embeddings existentes em {chembl_embeddings_path}")
-        chembl_embeddings = np.load(chembl_embeddings_path)
-    else:
-        print(f"Criando embeddings a partir do input {chembl_file_path}")
-        chembl_data = pd.read_csv(chembl_file_path, sep='\t')
-        chembl_smiles = chembl_data['canonical_smiles'].tolist()
-        similarity_finder = MoleculeSimilarityFinder(chembl_file_path, pkidb_file_path, device)
-        chembl_embeddings = similarity_finder.get_molecule_embedding(chembl_smiles).cpu().numpy()
-        np.save(chembl_embeddings_path, chembl_embeddings)
-
-
-    # Inicializar o localizador de similaridade
-    similarity_finder = MoleculeSimilarityFinder(chembl_file_path, pkidb_file_path, device)
-
-    # Carregar ou calcular embeddings do PKIDB
-    if os.path.exists(pkidb_file_path):
-        print(f"Carregando dados do PKIDB de {pkidb_file_path}")
-        pkidb_data = pd.read_csv(pkidb_file_path, header=None, names=['pkidb_smile'])
-        pkidb_smiles = pkidb_data['pkidb_smile'].tolist()
-        pkidb_embeddings = similarity_finder.get_molecule_embedding(pkidb_smiles).cpu().numpy()
-    else:
-        print("O arquivo PKIDB não foi encontrado.")
-        return
-
-    # Encontrar moléculas similares
-    similarity_scores, similar_molecules_info = similarity_finder.find_similar_molecules(pkidb_file_path, chembl_embeddings_path, threshold)
-
-    # Visualizações
-    molecule_visualizer = MoleculeVisualization()
-    molecule_visualizer.save_similar_molecules_to_tsv(similar_molecules_info, output_file_path)
-
-  # Parar o cronômetro e imprimir o tempo total
-    end_time = time.time()
-    print(f"Tempo total de execução: {end_time - start_time:.2f} segundos")
-
-    # Combina os embeddings para visualização
-    all_embeddings = np.concatenate([chembl_embeddings, pkidb_embeddings], axis=0)
-    labels = ['ChEMBL'] * len(chembl_embeddings) + ['PKIDB'] * len(pkidb_embeddings)
-
-    # Visualização t-SNE
-    molecule_visualizer.visualize_with_tsne(all_embeddings, labels)
-    molecule_visualizer.cluster_and_visualize(all_embeddings, num_clusters=3)
-    molecule_visualizer.plot_histogram(similarity_scores)
-
-
-if __name__ == "__main__":
-    main()
-  
 
