394ea6d5d853da05321eebd697346a018eb62112
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-10 18:45:26 -0300

    Update cvae_0.01.py

--- cvae_0.01.py
+++ cvae_0.01.py
@@ -157,3 +157,40 @@ def generate_molecule(cvae, z, tokenizer):
         # Vamos decodificar o primeiro exemplo do batch
         recon_smiles_decoded = token_ids_to_smiles(recon_smiles[0], tokenizer)
         return recon_smiles_decoded
+
+
+# Certifique-se de que todas as classes e funções necessárias estejam importadas ou definidas aqui.
+# Isso inclui CVAE, SmilesDataset, train_cvae, smiles_to_token_ids, generate_molecule.
+
+def main(smiles_input, pretrained_model_name, pkidb_file_path, num_epochs=10, batch_size=32):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Tokenizador e modelo pré-treinado são carregados
+    tokenizer = RobertaTokenizer.from_pretrained(pretrained_model_name)
+    vocab_size = tokenizer.vocab_size  # Obtenha o tamanho do vocabulário do tokenizador
+
+    # Instancia o CVAE com o modelo pré-treinado, dimensão latente e tamanho do vocabulário
+    cvae = CVAE(pretrained_model_name=pretrained_model_name, 
+                latent_dim=256, 
+                vocab_size=vocab_size, 
+                max_sequence_length=tokenizer.model_max_length).to(device)
+
+    # Prepara o dataset e o dataloader
+    dataset = SmilesDataset(pkidb_file_path, tokenizer, max_length=tokenizer.model_max_length)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    # Configura o otimizador
+    optimizer = optim.Adam(cvae.parameters(), lr=1e-3)
+
+    # Treina o CVAE
+    train_cvae(cvae, dataloader, optimizer, num_epochs, log_interval=10)
+
+    # Gera uma nova molécula
+    input_ids, attention_mask = smiles_to_token_ids(smiles_input, tokenizer)
+    input_ids, attention_mask = input_ids.to(device), attention_mask.to(device)
+    z = cvae.encode(input_ids, attention_mask)[0]  # Obtem apenas o mu (média) do espaço latente
+    z = z.unsqueeze(0)  # Simula um lote de tamanho 1 para compatibilidade de formato
+    generated_smile = generate_molecule(cvae, z, tokenizer)
+
+    print(f"Generated SMILES: {generated_smile}")
+
