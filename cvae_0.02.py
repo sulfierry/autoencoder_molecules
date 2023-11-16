9780952f135a66d61a7a90c5da9646b6454ed0f2
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-16 16:54:24 -0300

    Update cvae_0.02.py

--- cvae_0.02.py
+++ cvae_0.02.py
@@ -163,24 +163,6 @@ def generate_molecule(cvae, z, tokenizer, method='sampling', top_k=50):
 def token_ids_to_smiles(token_ids, tokenizer):
     return tokenizer.decode(token_ids.tolist(), skip_special_tokens=True)
 
-
-def postprocess_smiles(generated_smiles, reference_smile):
-    corrected_smiles = []
-    ref_mol = Chem.MolFromSmiles(reference_smile)
-
-    for smile in generated_smiles:
-        try:
-            mol = Chem.MolFromSmiles(smile)
-            if mol is not None:
-                AllChem.AlignMol(mol, ref_mol)
-                corrected_smiles.append(Chem.MolToSmiles(mol))
-            else:
-                corrected_smiles.append("Invalid SMILES")
-        except:
-            corrected_smiles.append("Error in Parsing")
-
-    return corrected_smiles
-
     
 class CVAE(nn.Module):
     def __init__(self, pretrained_model_name, latent_dim, vocab_size, max_sequence_length):
@@ -225,15 +207,52 @@ class CVAE(nn.Module):
         output = output.view(-1, self.max_sequence_length, self.vocab_size)
         # LogSoftmax já está sendo aplicado no nn.Sequential
         return output
-    
-        
-
-
 
     def forward(self, input_ids, attention_mask):
         mu, log_var = self.encode(input_ids, attention_mask)
         z = self.reparameterize(mu, log_var)
         return self.decode(z), mu, log_var
+
+
+def calculate_properties(mol):
+    return {
+        'mw': Descriptors.MolWt(mol),
+        'logp': Descriptors.MolLogP(mol),
+        'hbd': rdMolDescriptors.CalcNumHBD(mol),
+        'hba': rdMolDescriptors.CalcNumHBA(mol)
+    }
+
+def is_similar(prop1, prop2, threshold=0.2):
+    for key in prop1:
+        if abs(prop1[key] - prop2[key]) > threshold:
+            return False
+    return True
+
+def postprocess_smiles(smiles_list, reference_smile):
+    reference_mol = Chem.MolFromSmiles(reference_smile)
+    reference_properties = calculate_properties(reference_mol)
+    processed_smiles = []
+
+    for smile in smiles_list:
+        try:
+            mol = Chem.MolFromSmiles(smile)
+            if mol:
+                canonical_smile = Chem.MolToSmiles(mol, isomericSmiles=True)
+                largest_fragment = max(canonical_smile.split('.'), key=len)
+                mol = Chem.MolFromSmiles(largest_fragment)
+                
+                properties = calculate_properties(mol)
+                if is_similar(properties, reference_properties):
+                    processed_smiles.append({
+                        'smile': largest_fragment,
+                        **properties
+                    })
+            else:
+                processed_smiles.append({'smile': 'Invalid SMILES'})
+        except Exception as e:
+            processed_smiles.append({'smile': f"Error: {str(e)}"})
+
+    return processed_smiles
 
 
 
