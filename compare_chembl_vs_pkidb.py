6c2814c5a9b306448cf3bff5a04ce95a1028a8cb
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-23 10:46:40 -0300

    Update compare_chembl_vs_pkidb.py

--- compare_chembl_vs_pkidb.py
+++ compare_chembl_vs_pkidb.py
@@ -18,3 +18,10 @@ def process_chunk(chunk, pkidb_fingerprints):
 def smiles_to_fingerprint(smiles):
     mol = Chem.MolFromSmiles(smiles)
     return rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius=2)
+
+
+
+# Carregar os SMILES do pkidb
+pkidb_smiles = pd.read_csv('/mnt/data/pkidb_2023-06-30.tsv', sep='\t')
+pkidb_smiles['fingerprint'] = pkidb_smiles['Canonical_Smiles'].apply(smiles_to_fingerprint)
+
