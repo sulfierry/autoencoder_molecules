5a324ffe2fb867e7fd06235a0ce2511991a320a2
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-24 12:52:26 -0300

    Update cluster_pchembl_value.py

--- cluster_pchembl_value.py
+++ cluster_pchembl_value.py
@@ -8,3 +8,9 @@ import matplotlib.pyplot as plt
 # Carregar dados
 file_path = './kinase_ligands_pchembl_Value.tsv'
 data = pd.read_csv(file_path, sep='\t')
+
+
+# Função para converter SMILES em fingerprint
+def smiles_to_fingerprint(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    return AllChem.GetMorganFingerprintAsBitVect(mol, radius=2) if mol else None
