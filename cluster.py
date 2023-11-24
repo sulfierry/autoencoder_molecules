3ab211b5782ff0426f7f7870dfa9fe383294762c
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-24 20:00:54 -0300

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -53,7 +53,7 @@ class TSNEClusterer:
             return 'grupo5_(11 - 12)'
         else:
             return '>12'
-
+            
     def calculate_tsne_for_group(self, group_data):
         # Este método será chamado em paralelo para cada grupo
         fingerprints = [self.smiles_to_fingerprint(smiles) for smiles in group_data['smiles'] if smiles]
@@ -83,47 +83,45 @@ class TSNEClusterer:
             return tsne_result
         return None
             
+
     def calculate_tsne(self):
         # Preparar dados para t-SNE e plotagem
         tsne_results = []
         group_labels = []
-    
-        # Definir uma função interna para calcular t-SNE para um grupo específico
-        def calculate_tsne_for_group(self, group_data):
-            fingerprints = [self.smiles_to_fingerprint(smiles) for smiles in group_data['smiles'] if smiles]
-            fingerprints_matrix = np.array([fp for fp in fingerprints if fp is not None])
-            if len(fingerprints_matrix) > 5:
-                tsne = TSNE(n_components=2, random_state=0, perplexity=min(30, len(fingerprints_matrix) - 1))
-                tsne_result = tsne.fit_transform(fingerprints_matrix)
-                return tsne_result, group_data['pchembl_group'].iloc[0]
 
-    
-        # Utilizar todas as CPUs disponíveis para calcular t-SNE em paralelo para cada grupo
+        # Processamento paralelo para calcular t-SNE para cada grupo de pChEMBL
         with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-            # Criar uma lista de tarefas para execução paralela
-            futures = [executor.submit(calculate_tsne_for_group, self.data[self.data['pchembl_group'] == group], group)
-                       for group in self.data['pchembl_group'].unique()]
-            
-            # Processar os resultados assim que cada tarefa for concluída
+            futures = []
+            for group in self.data['pchembl_group'].unique():
+                group_data = self.data[self.data['pchembl_group'] == group]
+                futures.append(executor.submit(self.calculate_tsne_for_group, group_data))
+
             for future in concurrent.futures.as_completed(futures):
-                try:
-                    result, group = future.result()
-                    if result is not None:
-                        tsne_results.extend(result)
-                        group_labels.extend([group] * len(result))
-                except Exception as e:
-                    print(f"Erro durante a execução da tarefa: {e}")
-    
-        # Organizar os resultados do t-SNE em um DataFrame
+                result, group = future.result()
+                if result is not None:
+                    tsne_results.extend(result)
+                    group_labels.extend([group] * len(result))
+
+        # Adicionar os resultados t-SNE ao DataFrame
         self.tsne_df = pd.DataFrame(tsne_results, columns=['x', 'y'])
         self.tsne_df['group'] = group_labels
-    
+
         # Calcular t-SNE para dados PKIDB
         fingerprints_pkidb = np.array([list(fp) for fp in self.pkidb_data['fingerprint']])
         tsne_pkidb = TSNE(n_components=2, random_state=0, perplexity=30)
         tsne_results_pkidb = tsne_pkidb.fit_transform(fingerprints_pkidb)
+
+        # Adicionar os resultados t-SNE do PKIDB ao DataFrame
         self.pkidb_data['x'] = tsne_results_pkidb[:, 0]
         self.pkidb_data['y'] = tsne_results_pkidb[:, 1]
+
+    def calculate_tsne_for_group(self, group_data):
+        fingerprints = [self.smiles_to_fingerprint(smiles) for smiles in group_data['smiles'] if smiles]
+        fingerprints_matrix = np.array([fp for fp in fingerprints if fp is not None])
+        if len(fingerprints_matrix) > 5:
+            tsne = TSNE(n_components=2, random_state=0, perplexity=min(30, len(fingerprints_matrix) - 1))
+            tsne_result = tsne.fit_transform(fingerprints_matrix)
+            return tsne_result, group_data['pchembl_group'].iloc[0]
 
         
     def plot_tsne(self):
