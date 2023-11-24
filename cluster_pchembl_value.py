dffa1d19f6c6b084e60239c9ca17234b4f18e70d
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-24 12:51:34 -0300

    Create cluster_pchembl_value.py

---
+++ cluster_pchembl_value.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+# Carregar dados
+file_path = './kinase_ligands_pchembl_Value.tsv'
+data = pd.read_csv(file_path, sep='\t')
