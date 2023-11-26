65d2f99d41b2e51ba6ae331279b6d7e494899de1
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-25 21:17:45 -0300

    Update cluster_2.py

--- cluster_2.py
+++ cluster_2.py
@@ -2,10 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 from rdkit import Chem
+import concurrent.futures
 from rdkit.Chem import AllChem
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from concurrent.futures import ProcessPoolExecutor
+
 
 class TSNEClusterer:
     def __init__(self, data_path, pkidb_path):
@@ -13,6 +15,8 @@ class TSNEClusterer:
         self.pkidb_path = pkidb_path
         self.data = None
         self.pkidb_data = None
+        self.tsne_results = []  # Inicializa a lista aqui
+        self.group_labels = []  # Inicializa a lista aq
 
     def load_data(self):
         self.data = pd.read_csv(self.data_path, sep='\t')
@@ -22,19 +26,19 @@ class TSNEClusterer:
     def smiles_to_fingerprint(smiles):
         try:
             mol = Chem.MolFromSmiles(smiles)
-            if mol:
+            if mol and mol.GetNumAtoms() > 0 and Chem.SanitizeMol(mol, catchErrors=True) == Chem.SanitizeFlags.SANITIZE_NONE:
                 return AllChem.GetMorganFingerprintAsBitVect(mol, radius=2)
         except Exception as e:
             print(f"Erro ao converter SMILES: {smiles} - {e}")
         return None
 
+
+
     def preprocess_data(self):
         self.data['pchembl_group'] = self.data['pchembl_value'].apply(self.pchembl_group)
         self.pkidb_data['fingerprint'] = self.pkidb_data['Canonical_Smiles'].apply(self.smiles_to_fingerprint)
         self.pkidb_data.dropna(subset=['fingerprint'], inplace=True)
-
-    # Restante das funções da classe...
-
+      
     
     def pchembl_group(self, value):
         if pd.isna(value):
@@ -80,44 +84,60 @@ class TSNEClusterer:
             tsne_result = tsne.fit_transform(fingerprints_matrix)
             return tsne_result
         return None
-  
+        
     def calculate_tsne(self):
         # Preparar dados para t-SNE e plotagem
         tsne_results = []
         group_labels = []
-    
+        
         # Processamento paralelo para calcular t-SNE para cada grupo de pChEMBL
         with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
             futures = []
             for group in self.data['pchembl_group'].unique():
                 group_data = self.data[self.data['pchembl_group'] == group]
-                futures.append(executor.submit(self.process_group_data, group_data['Canonical_Smiles']))
-    
+                futures.append(executor.submit(self.calculate_tsne_for_group, group_data))
+        
             for future in concurrent.futures.as_completed(futures):
-                result = future.result()
+                result, group = future.result()
                 if result is not None:
-                    tsne_results.extend(result[0])
-                    group_labels.extend([result[1]] * len(result[0]))
-    
+                    tsne_results.extend(result)
+                    group_labels.extend([group] * len(result))
+        
         # Adicionar os resultados t-SNE ao DataFrame
         self.tsne_df = pd.DataFrame(tsne_results, columns=['x', 'y'])
         self.tsne_df['group'] = group_labels
-    
+        
         # Calcular t-SNE para dados PKIDB
-        self.calculate_tsne_pkidb()
-
-    def calculate_tsne_pkidb(self):
         fingerprints_pkidb = np.array([list(fp) for fp in self.pkidb_data['fingerprint'] if fp is not None])
+        
+        # Verificar a quantidade de fingerprints válidos para PKIDB
+        print(f"Número de fingerprints válidos para PKIDB: {len(fingerprints_pkidb)}")
+        
         if len(fingerprints_pkidb) > 0:
             tsne_pkidb = TSNE(n_components=2, random_state=0, perplexity=30)
             tsne_results_pkidb = tsne_pkidb.fit_transform(fingerprints_pkidb)
-    
+        
             # Adicionar os resultados t-SNE do PKIDB ao DataFrame
             self.pkidb_data['x'] = tsne_results_pkidb[:, 0]
             self.pkidb_data['y'] = tsne_results_pkidb[:, 1]
+            
+            # Verificar se as colunas 'x' e 'y' foram adicionadas corretamente
+            if 'x' in self.pkidb_data.columns and 'y' in self.pkidb_data.columns:
+                print("Colunas 'x' e 'y' adicionadas com sucesso ao DataFrame pkidb_data.")
+            else:
+                print("Falha ao adicionar colunas 'x' e 'y' ao DataFrame pkidb_data.")
         else:
             print("Nenhum fingerprint válido encontrado para PKIDB.")
 
+
+
+    def calculate_tsne_for_group(self, group_data):
+        fingerprints = [self.smiles_to_fingerprint(smiles) for smiles in group_data['smiles'] if smiles]
+        fingerprints_matrix = np.array([fp for fp in fingerprints if fp is not None])
+        if len(fingerprints_matrix) > 5:
+            tsne = TSNE(n_components=2, random_state=0, perplexity=min(30, len(fingerprints_matrix) - 1))
+            tsne_result = tsne.fit_transform(fingerprints_matrix)
+            return tsne_result, group_data['pchembl_group'].iloc[0]
 
     def plot_tsne(self):
         plt.figure(figsize=(12, 8))
@@ -176,9 +196,33 @@ class TSNEClusterer:
         self.plot_tsne()
         self.save_data()
 
+      
+def calculate_tsne_parallel(tsne_clusterer, group):
+    group_data = tsne_clusterer.data[tsne_clusterer.data['pchembl_group'] == group]
+    return tsne_clusterer.calculate_tsne_for_group(group_data)
+
+
 def main():
-    tsne_clusterer = TSNEClusterer('./kinase_ligands_pchembl_Value.tsv', './PKIDB/pkidb_2023-06-30.tsv')
-    tsne_clusterer.run()
+    tsne_clusterer = TSNEClusterer('./kinase_ligands_pchembl_Value.tsv', '../PKIDB/pkidb_2023-06-30.tsv')
+    tsne_clusterer.load_data()
+    tsne_clusterer.preprocess_data()
+    
+    # Utilizar todas as CPUs disponíveis para calcular t-SNE em paralelo para cada grupo
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        futures = {executor.submit(calculate_tsne_parallel, tsne_clusterer, group): group for group in tsne_clusterer.data['pchembl_group'].unique()}
+        
+        for future in concurrent.futures.as_completed(futures):
+            result, group = future.result()
+            if result is not None:
+                tsne_clusterer.tsne_results.extend(result)
+                tsne_clusterer.group_labels.extend([group] * len(result))
+    
+    tsne_clusterer.tsne_df = pd.DataFrame(tsne_clusterer.tsne_results, columns=['x', 'y'])
+    tsne_clusterer.tsne_df['group'] = tsne_clusterer.group_labels
+    
+    # Continuar com o restante da execução
+    tsne_clusterer.plot_tsne()
+    tsne_clusterer.save_data()
 
 if __name__ == '__main__':
     main()
