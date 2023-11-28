0a97057f8871f9ebfd0861bd92f32ddc451152df
Leon Sulfierry <leon.bio0@hotmail.com>
2023-11-28 11:50:43 -0300

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -5,3 +5,9 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, DataStructs
 from scipy.spatial.distance import cosine
 import seaborn as sns
+
+# Funções auxiliares
+def smiles_to_fingerprint(smiles, radius=2):
+    """Converte SMILES para fingerprint de Morgan."""
+    mol = Chem.MolFromSmiles(smiles)
+    return AllChem.GetMorganFingerprintAsBitVect(mol, radius) if mol else None
