f2b5b5b5edef728ae42953933d4dfcb241dae029
Leon Sulfierry <leon.bio0@hotmail.com>
2023-12-04 10:24:49 -0300

    Update cluster_by_similarity.py

--- cluster_by_similarity.py
+++ cluster_by_similarity.py
@@ -8,6 +8,9 @@ from multiprocessing import Pool
 from tqdm import tqdm
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
+import matplotlib.cm as cm
+from rdkit.Chem import Descriptors
 
 class MoleculeClusterer:
     def __init__(self, smiles_file_path):
@@ -24,22 +27,40 @@ class MoleculeClusterer:
     def preprocess_data(self):
         # Classifica os valores de pchembl em grupos
         self.data['pchembl_group'] = self.data['pchembl_value'].apply(self.pchembl_group)
+    
+    def cluster_by_similarity(self, threshold=0.8):
+        num_fps = len(self.fingerprints)
+        clusters = []
+        visited = set()
 
+        for i in range(num_fps):
+            if i in visited:
+                continue
+
+            cluster = [i]
+            for j in range(i + 1, num_fps):
+                similarity = DataStructs.TanimotoSimilarity(self.fingerprints[i], self.fingerprints[j])
+                if similarity >= threshold:
+                    cluster.append(j)
+                    visited.add(j)
+
+            clusters.append(cluster)
+
+        return clusters
+    
     @staticmethod
     def pchembl_group(value):
         # Lógica para classificar o valor pchembl em grupos
         if pd.isna(value):
             return 'sem_pchembl'
-        elif 1 < value < 8:
+        elif 1 < value < 9:
             return 'grupo1'
-        elif 8 <= value < 9:
-            return 'grupo2'
         elif 9 <= value < 10:
-            return 'grupo3'
+            return 'grupo2'
         elif 10 <= value < 11:
-            return 'grupo4'
+            return 'grupo3'
         elif 11 <= value < 12:
-            return 'grupo5'
+            return 'grupo4'
         else:
             return '>12'
 
@@ -57,14 +78,6 @@ class MoleculeClusterer:
         with Pool(num_cpus) as pool:
             self.fingerprints = list(tqdm(pool.imap(self.compute_fingerprint, self.data['canonical_smiles']), total=len(self.data)))
         self.fingerprints = [fp for fp in self.fingerprints if fp is not None]
-
-    def calculate_similarity_matrix(self):
-        num_fps = len(self.fingerprints)
-        similarity_matrix = np.zeros((num_fps, num_fps))
-        for i in tqdm(range(num_fps)):  # Adicionando barra de progresso aqui
-            for j in range(num_fps):
-                similarity_matrix[i, j] = DataStructs.TanimotoSimilarity(self.fingerprints[i], self.fingerprints[j])
-        return similarity_matrix
 
     def save_clustered_data(self, output_file_path):
         self.data.to_csv(output_file_path, sep='\t', index=False)
@@ -91,44 +104,90 @@ class MoleculeClusterer:
         tsne_results = tsne.fit_transform(fingerprint_array)
         return tsne_results
 
-
-    def plot_tsne(self, tsne_results, group_labels):
+    def plot_tsne(self, tsne_results):
         plt.figure(figsize=(12, 8))
 
-        # Cores e ordem de plotagem baseada nos grupos pchembl
-        group_to_color = {
-            'sem_pchembl': 'grey',
-            'grupo1': 'blue',
-            'grupo2': 'green',
-            'grupo3': 'yellow',
-            'grupo4': 'orange',
-            'grupo5': 'purple',
-            '>12': 'red'
-        }
+        # Certifique-se de que os IDs dos clusters são inteiros
+        cluster_ids = self.data['ClusterID'].astype(int)
 
-        for group, color in group_to_color.items():
-            # Filtrar os resultados do t-SNE pelo grupo atual
-            indices = [i for i, g in enumerate(group_labels) if g == group]
-            subset = tsne_results[indices, :]
-            plt.scatter(subset[:, 0], subset[:, 1], color=color, label=group, alpha=0.5)
+        # Obter o número de clusters únicos
+        num_clusters = len(set(cluster_ids))
 
-        plt.legend()
+        # Criar o mapa de cores
+        cmap = plt.cm.viridis
+
+        # Obter o tamanho de cada cluster
+        cluster_sizes = self.data['ClusterID'].value_counts().sort_index()
+        
+        # Mapear cada cluster ID para o tamanho do cluster
+        size_map = cluster_sizes.to_dict()
+        cluster_colors = np.array([size_map[x] for x in cluster_ids])
+
+        # Plotagem do TSNE com a coloração do cluster
+        scatter = plt.scatter(tsne_results[:, 0], tsne_results[:, 1], c=cluster_colors, cmap=cmap, alpha=0.5)
+
+        # Criar um objeto de barra de cores com um número inteiro de bins
+        cbar = plt.colorbar(scatter, spacing='proportional', ticks=range(cluster_sizes.min(), cluster_sizes.max() + 1))
+        cbar.set_label('Cluster Size')
+
         plt.title('Visualização 2D de Similaridade Molecular com Clusters')
         plt.xlabel('t-SNE Dimensão 1')
         plt.ylabel('t-SNE Dimensão 2')
         plt.show()
+        
+    def plot_cluster_size_distribution(self):
+        # Contar o número de smiles em cada cluster
+        cluster_sizes = self.data['ClusterID'].value_counts()
+
+        # Criar um histograma dos tamanhos dos clusters
+        plt.figure(figsize=(10, 6))
+        plt.hist(cluster_sizes, bins=range(1, cluster_sizes.max() + 1), alpha=0.7, color='blue', edgecolor='black')
+
+        plt.title('Distribuição do Número de Smiles por Cluster')
+        plt.xlabel('Número de Smiles no Cluster')
+        plt.ylabel('Contagem de Clusters')
+        plt.grid(axis='y', alpha=0.75)
+
+        # Mostrar média e mediana
+        mean_size = cluster_sizes.mean()
+        median_size = cluster_sizes.median()
+        plt.axvline(mean_size, color='red', linestyle='dashed', linewidth=1, label=f'Média: {mean_size:.2f}')
+        plt.axvline(median_size, color='green', linestyle='dashed', linewidth=1, label=f'Mediana: {median_size:.2f}')
+
+        plt.legend()
+        plt.show()
+
+    def save_clusters_as_tsv(self, threshold=5):
+        # Criar pasta 'clusters' se não existir
+        os.makedirs('./clusters', exist_ok=True)
+
+        # Colunas a serem incluídas no arquivo de saída
+        output_columns = ['molregno', 'kinase_alvo', 'canonical_smiles', 'standard_value', 'standard_type', 'pchembl_value',
+                        'nome_medicamento', 'ClusterID']
+
+        # Calcula o peso molecular para cada SMILES e adiciona como uma nova coluna
+        self.data['molecular_weight'] = self.data['canonical_smiles'].apply(lambda x: Descriptors.MolWt(Chem.MolFromSmiles(x)) if x else None)
+
+        for cluster_id in set(self.data['ClusterID']):
+            cluster_data = self.data[self.data['ClusterID'] == cluster_id]
+
+            # Ordenar os dados do cluster por peso molecular
+            cluster_data_sorted = cluster_data.sort_values('molecular_weight')
+
+            # Salvar apenas se o cluster contiver pelo menos 'threshold' SMILES
+            if len(cluster_data_sorted) >= threshold:
+                # Selecionar apenas as colunas desejadas para salvar
+                cluster_data_to_save = cluster_data_sorted[output_columns]
+                cluster_data_to_save.to_csv(f'./clusters/cluster_{cluster_id}.tsv', sep='\t', index=False)
 
 
 def main():
-    # Caminhos dos arquivos
     smiles_file_path = './nr_kinase_drug_info_kd_ki_manually_validated.tsv'
     output_file_path = './clustered_smiles.tsv'
     state_file_path = './molecule_clusterer_state.pkl'
 
-    # Instanciar a classe MoleculeClusterer
     clusterer = MoleculeClusterer(smiles_file_path)
 
-    # Tentar carregar o estado salvo anteriormente
     try:
         clusterer.load_state(state_file_path)
         print("Estado carregado com sucesso.")
@@ -142,24 +201,22 @@ def main():
         print(f"Erro ao carregar o estado: {e}")
         return
 
-    # Realizar cálculos de similaridade e visualização com base nos grupos de pchembl_value
     if clusterer.fingerprints:
-
-        # TSNE calculation
         tsne_results = clusterer.calculate_tsne()
+        clusters = clusterer.cluster_by_similarity(threshold=0.8)
+        clusterer.save_clusters_as_tsv(threshold=5)
 
-        # Get group labels for plotting (ensure this matches with the tsne_results)
-        group_labels = clusterer.data['pchembl_group'].values
+        cluster_ids = [None] * len(clusterer.data)
+        for cluster_id, cluster in enumerate(clusters):
+            for idx in cluster:
+                cluster_ids[idx] = cluster_id
+        clusterer.data['ClusterID'] = cluster_ids
 
-        # Visualização dos resultados com cores baseadas nos grupos pchembl
-        clusterer.plot_tsne(tsne_results, group_labels)
-        
-        # Salvar os dados clusterizados
+        clusterer.plot_tsne(tsne_results)
+        clusterer.plot_cluster_size_distribution()
+
         clusterer.save_clustered_data(output_file_path)
-
-        # Salvar o estado final
         clusterer.save_state(state_file_path)
-
     else:
         print("Nenhum fingerprint válido foi encontrado.")
 
