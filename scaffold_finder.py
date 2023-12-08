d980e72b09e09149eaa279965d770a862d272210
Leon Sulfierry <leon.bio0@hotmail.com>
2023-12-08 12:44:26 -0300

    Update scaffold_finder.py

--- scaffold_finder.py
+++ scaffold_finder.py
@@ -9,3 +9,11 @@ def get_scaffold(smiles):
     molecule = Chem.MolFromSmiles(smiles)
     scaffold = MurckoScaffold.GetScaffoldForMol(molecule)
     return Chem.MolToSmiles(scaffold) if scaffold else None
+
+# Função para calcular a similaridade de Tanimoto
+def calculate_similarity(smiles1, smiles2):
+    mol1 = Chem.MolFromSmiles(smiles1)
+    mol2 = Chem.MolFromSmiles(smiles2)
+    fp1 = AllChem.GetMorganFingerprint(mol1, 2)
+    fp2 = AllChem.GetMorganFingerprint(mol2, 2)
+    return DataStructs.TanimotoSimilarity(fp1, fp2)
