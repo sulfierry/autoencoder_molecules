c761b9e385c8746e7c4f10ec00d3b16841217ee7
Leon Sulfierry <leon.bio0@hotmail.com>
2023-12-11 16:21:53 -0300

    Update chembl_from_smile_to_sdf.py

--- chembl_from_smile_to_sdf.py
+++ chembl_from_smile_to_sdf.py
@@ -1,12 +1,21 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import pandas as pd
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 # Carregar seu arquivo TSV
 data = pd.read_csv('../1_database/nr_kinase_drug_info_kd_ki_ic50_10uM_manually_validated.tsv', sep='\t')
 
+
+# Criar uma pasta para os arquivos .sdf se não existir
+output_folder = 'smiles_to_sdf'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 # Função para converter SMILES em arquivos .sdf
-def smiles_to_sdf(smiles, file_name):
+def smiles_to_sdf(smiles, molregno):
+    file_name = os.path.join(output_folder, f'mol_{molregno}.sdf')
     # Converter SMILES para um objeto de molécula
     mol = Chem.MolFromSmiles(smiles)
 
@@ -19,10 +28,3 @@ def smiles_to_sdf(smiles, file_name):
         # Criar um escritor SDF e escrever a molécula
         with Chem.SDWriter(file_name) as writer:
             writer.write(mol)
-
-# Converter cada SMILES da coluna 'canonical_smiles' em um arquivo .sdf
-for index, row in data.iterrows():
-    smiles = row['canonical_smiles']
-    molregno = row['molregno']
-    file_name = f'mol_{molregno}.sdf'
-    smiles_to_sdf(smiles, file_name)
